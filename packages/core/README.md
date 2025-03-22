a66cf88a37564e7a7eb8e991de632560476b1293
Meng Zhang <meng@tabbyml.com>
2025-03-22 15:17:13 +0800

    remove core

--- packages/core/README.md
+++
@@ -1,3 +0,0 @@
-# @ragdoll/core
-
-Provides core intelligence for Ragdoll, e.g the interaction with LLM, Tool Use, etc.
--- packages/core/src/prompts/__tests__/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "bun:test"
-import { generatePrompt } from ".."
-
-test("snapshot of generatePrompt", async () => {
-    const prompt = await generatePrompt()
-    expect(prompt).toMatchSnapshot()
-})
