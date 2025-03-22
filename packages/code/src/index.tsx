ffd9024fc263d8aa23316f39a4484c0bc870a936
Meng Zhang <meng@tabbyml.com>
2025-03-22 22:23:47 +0800

    update

--- packages/code/src/index.tsx
+++ packages/code/src/index.tsx
@@ -1,28 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import { Box, render, Text, useInput, useStdin } from 'ink';
 import TextInput from 'ink-text-input';
-import { useChat } from "@ai-sdk/react"
+import { useChat } from "@ai-sdk/react";
+import type { ToolInvocation } from '@ai-sdk/ui-utils';
 
 const App = () => {
-	const { messages, handleSubmit, input, setInput } = useChat({
-		api: "http://localhost:4111/api/agents/tabby/stream",
-		maxSteps: 2,
-		onToolCall: (tool) => {
-			console.log(tool);
-			return {
-				files: ["file1.txt", "file2.txt"],
-			}
-		}
-	})
+  const { messages, handleSubmit, input, setInput } = useChat({
+    api: "http://localhost:4111/api/agents/tabby/stream",
+    maxSteps: 2,
+    onToolCall: (tool) => {
+      return {
+        files: ["file1.txt", "file2.txt"],
+      };
+    },
+  });
 
-	return (
-		<Box flexDirection="column">
-			{messages.map((message, index) => (
-				<Box key={index}><Text>{index}: {message.content}</Text></Box>
-			))}
-			<TextInput value={input} onChange={setInput} onSubmit={() => handleSubmit()} />
-		</Box>
-	);
+  return (
+    <Box flexDirection="column" padding={1}>
+      {messages.length > 0 && <Box flexDirection="column" borderStyle="round" borderColor="cyan" padding={1}>
+        {messages.map((message, index) => (
+          <Box key={index} marginBottom={1}>
+            {message.parts.map((part, index) => {
+              if (part.type === "text") {
+                return <MessageText key={index} text={part.text} role={message.role} />;
+              } else if (part.type === "tool-invocation") {
+                return <MessageToolInvocation key={index} toolInvocation={part.toolInvocation} />
+              }
+            })}
+          </Box>
+        ))}
+      </Box>}
+
+      <Box marginTop={1}>
+        <TextInput
+          value={input}
+          onChange={setInput}
+          onSubmit={() => handleSubmit()}
+          placeholder="Type your message here..."
+        />
+      </Box>
+    </Box>
+  );
 };
+
+function MessageText({ text, role }: { text: string, role: string }) {
+  return <Text color={role === "user" ? "blue" : "yellow"}>
+    {role === "user" ? "You" : "Tabby"}: {text}
+  </Text>
+}
+
+function MessageToolInvocation({ toolInvocation }: {toolInvocation: ToolInvocation}) {
+  return <Text color="green">
+    {toolInvocation.name} is being invoked...
+  </Text>
+}
 
 render(<App />);
