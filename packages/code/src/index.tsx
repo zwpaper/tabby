f2eff4059ac3e2b9af844702ae4d2204a568ec54
Meng Zhang <meng@tabbyml.com>
2025-03-24 13:18:51 +0800

    improve ui a bit

--- packages/code/src/index.tsx
+++ packages/code/src/index.tsx
@@ -1,26 +1,44 @@
 import { useChat } from "@ai-sdk/react";
 import type { ToolInvocation } from "@ai-sdk/ui-utils";
 import { Box, Text, render } from "ink";
-import { TextInput } from "@inkjs/ui";
+import { Alert, Spinner, TextInput } from "@inkjs/ui";
 import Markdown from "./components/markdown";
 import { onToolCall } from "./tools";
 
 const App = () => {
-  const { messages, handleSubmit, setInput } = useChat({
+  const { messages, handleSubmit, setInput, status } = useChat({
     api: "http://localhost:4111/api/chat/stream",
     maxSteps: 2,
     onToolCall,
   });
 
+  const isLoading = status === "submitted" || status === "streaming";
+
+  let renderMessages = [...messages];
+  if (isLoading && messages[messages.length - 1]?.role !== "assistant") {
+    renderMessages.push({
+      id: "",
+      role: "assistant",
+      content: "",
+      parts: [],
+    });
+  }
+
   return (
     <Box flexDirection="column" padding={1}>
+      <Box width={30} marginBottom={2}>
+        <Alert variant="info">Tabby Code is in beta.</Alert>
+      </Box>
       {messages.length > 0 && (
-        <Box flexDirection="column" gap={1}>
-          {messages.map((message) => (
+        <Box marginBottom={1} marginLeft={2} flexDirection="column" gap={1}>
+          {renderMessages.map((message, index) => (
             <Box key={message.id} flexDirection="column" gap={1}>
-              <Text color={getRoleColor(message.role)}>
-                {message.role === "user" ? "You" : "Tabby"}
-              </Text>
+              <Box gap={1}>
+                <Text color={getRoleColor(message.role)}>
+                  {message.role === "user" ? "You" : "Tabby"}
+                </Text>
+                {isLoading && index === renderMessages.length - 1 && <Spinner />}
+              </Box>
               {message.parts.map((part, index) => {
                 if (part.type === "text") {
                   return <MessageText key={index} text={part.text} />;
@@ -39,16 +57,15 @@ const App = () => {
         </Box>
       )}
 
-      <Box marginTop={1} borderStyle="round" borderColor="white" padding={1}>
-        <Box>
+      {!isLoading &&
+        <Box borderStyle="round" borderColor="white" padding={1}>
           <TextInput
             key={messages.length + 1}
             onChange={setInput}
             onSubmit={() => handleSubmit()}
             placeholder="Type your message here..."
           />
-        </Box>
-      </Box>
+        </Box>}
     </Box>
   );
 };
