d604d7105f6327b4b563c79214fc75fcd300b0df
Meng Zhang <meng@tabbyml.com>
2025-04-06 16:48:02 +0800

    chore(db): switch from NeonDialect to PostgresDialect

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -1,11 +1,20 @@
-import { Kysely } from "kysely";
-import { NeonDialect } from "kysely-neon";
-import ws from "ws";
+import { Kysely, PostgresDialect } from "kysely";
+import { Pool } from "pg";
+import { parse } from "pg-connection-string";
 import type { DB } from "./schema";
 
 export const db = new Kysely<DB>({
-  dialect: new NeonDialect({
-    connectionString: process.env.DATABASE_URL,
-    webSocketConstructor: ws,
+  dialect: new PostgresDialect({
+    pool: async () => {
+      const conn = parse(process.env.DATABASE_URL || "");
+      return new Pool({
+        database: conn.database ?? undefined,
+        password: conn.password,
+        user: conn.user,
+        host: conn.host ?? undefined,
+        port: Number.parseInt(conn.port ?? "5432", 10),
+        ssl: !!conn.ssl,
+      });
+    },
   }),
 });
