dfedbf06d91f972a41d6dc72a62ed06f16549ba6
Meng Zhang <meng@tabbyml.com>
2025-04-18 12:15:36 +0800

    fix build

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -1,9 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import Ragdoll from "./Ragdoll";
 import { Extension } from "./helpers/extension";
 import uriHandler from "./helpers/uri-handler";
+import Ragdoll from "./ragdoll";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
