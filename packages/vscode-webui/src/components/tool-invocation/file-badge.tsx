6f6e2be8203ea7a9978e020045ca9de8b7201357
Meng Zhang <meng@tabbyml.com>
2025-05-06 17:42:05 -0700

    style(tool-invocation): update styling and formatting of FileBadge component

--- packages/vscode-webui/src/components/tool-invocation/file-badge.tsx
+++ packages/vscode-webui/src/components/tool-invocation/file-badge.tsx
@@ -17,8 +17,8 @@ export const FileBadge: React.FC<FileBadgeProps> = ({
 }) => {
   const lineRange = startLine
     ? endLine
-      ? `: ${startLine}-${endLine}`
-      : `: ${startLine}`
+      ? `:${startLine}-${endLine}`
+      : `:${startLine}`
     : "";
   const onClick = () => {
     vscodeHost.openFile(path, startLine ? { line: startLine } : undefined);
@@ -28,14 +28,14 @@ export const FileBadge: React.FC<FileBadgeProps> = ({
     <span
       onClick={onClick}
       className={cn(
-        "inline-block cursor-pointer rounded-sm border border-zinc-600 px-1 text-xs text-zinc-400 active:bg-zinc-700",
+        "inline-block cursor-pointer rounded-sm border border-zinc-600 px-1 text-xs active:bg-zinc-700",
         className,
       )}
     >
       <File className="inline-block size-3" />
       <span className="ml-1">
         {path}
-        {lineRange}
+        <span className="text-zinc-400">{lineRange}</span>
       </span>
     </span>
   );
