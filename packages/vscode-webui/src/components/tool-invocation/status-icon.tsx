16bd1d8370da1e6f2f73d6503444cc2f2d598400
zhanba <c5e1856@gmail.com>
2025-05-15 16:09:59 +0800

    fix(webui): add dev mode menu on status icon (#218)

--- packages/vscode-webui/src/components/tool-invocation/status-icon.tsx
+++ packages/vscode-webui/src/components/tool-invocation/status-icon.tsx
@@ -4,9 +4,19 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { useCopyToClipboard } from "@/lib/hooks/use-copy-to-clipboard";
+import { useIsDevMode } from "@/lib/hooks/use-is-dev-mode";
 import { cn } from "@/lib/utils";
 import type { ToolInvocation } from "ai";
-import { Check, CircleSmall, Loader2, Pause, X } from "lucide-react";
+import {
+  Check,
+  CheckIcon,
+  CircleSmall,
+  FilesIcon,
+  Loader2,
+  Pause,
+  X,
+} from "lucide-react";
 
 interface StatusIconProps {
   tool: ToolInvocation;
@@ -15,6 +25,8 @@ interface StatusIconProps {
 }
 
 export function StatusIcon({ tool, isExecuting, className }: StatusIconProps) {
+  const isDevMode = useIsDevMode();
+  const { isCopied, copyToClipboard } = useCopyToClipboard({ timeout: 2000 });
   let error: string | undefined;
   if (
     tool.state === "result" &&
@@ -24,22 +36,37 @@ export function StatusIcon({ tool, isExecuting, className }: StatusIconProps) {
     error = tool.result.error;
   }
 
+  const devButton = (
+    <span
+      onClick={() => copyToClipboard(JSON.stringify(tool, null, 2))}
+      className="my-1 flex cursor-pointer items-center rounded px-2 py-1 hover:bg-zinc-100 dark:text-zinc-400 dark:hover:bg-zinc-800"
+    >
+      {isCopied ? (
+        <CheckIcon
+          size={12}
+          className="inline text-green-700 text-sm dark:text-green-500"
+        />
+      ) : (
+        <FilesIcon className="inline" size={12} />
+      )}
+      <span className="ml-2 text-sm">Copy Tool Result</span>
+    </span>
+  );
+
+  const tooltipContent = [];
+
+  if (isDevMode) {
+    tooltipContent.push(devButton);
+  }
+
   let statusIcon = (
     <Pause className="size-4 text-zinc-500 dark:text-zinc-400" />
   );
   if (error) {
     statusIcon = (
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <X className="size-4 cursor-help text-red-500 dark:text-red-400" />
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>{error}</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+      <X className="size-4 cursor-help text-red-500 dark:text-red-400" />
     );
+    tooltipContent.push(<p>{error}</p>);
   } else if (tool.state === "result") {
     statusIcon = (
       <Check className="size-4 text-emerald-700 dark:text-emerald-300" />
@@ -53,6 +80,22 @@ export function StatusIcon({ tool, isExecuting, className }: StatusIconProps) {
       <Loader2 className="size-4 animate-spin text-zinc-500 dark:text-zinc-400" />
     );
   }
+
+  if (tooltipContent.length > 0) {
+    statusIcon = (
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>{statusIcon}</TooltipTrigger>
+          <TooltipContent>
+            {tooltipContent.map((item, index) => (
+              <div key={index}>{item}</div>
+            ))}
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    );
+  }
+
   return (
     <div className={cn("inline-block align-sub", className)}>{statusIcon}</div>
   );
