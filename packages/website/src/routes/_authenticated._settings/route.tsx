bfebc463335f51c72ad9cfc1215553fd2ce8118a
Meng Zhang <meng@tabbyml.com>
2025-05-24 19:39:15 -0700

    fix(settings): remove unnecessary utility class from layout

--- packages/website/src/routes/_authenticated._settings/route.tsx
+++ packages/website/src/routes/_authenticated._settings/route.tsx
@@ -89,7 +89,7 @@ function Settings() {
         <SiteHeader title={activePane?.title} />
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="w-full p-2 md:p-6 [&>*]:mx-auto">
+            <div className="w-full p-2 md:p-6">
               <Outlet />
             </div>
           </div>
