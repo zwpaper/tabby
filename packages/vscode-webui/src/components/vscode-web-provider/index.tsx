4063a871d56cf6ddae28822dc74bc82ba89c4502
Meng Zhang <meng@tabbyml.com>
2025-06-02 20:41:44 -0700

    fix: fix vscode-web-provider style inlining (#472)
    * refactor: update VSCodeWebProvider to use inline styles for improved performance
    
    * update
    
    * fix: reorder import statements for consistency in VSCodeWebProvider

--- packages/vscode-webui/src/components/vscode-web-provider/index.tsx
+++ packages/vscode-webui/src/components/vscode-web-provider/index.tsx
@@ -1,9 +1,19 @@
-import "./style.css";
+import styleDefault from "./style.css?inline";
 
-import "./vscode-default.css";
-import "./vscode-dark.css";
-import "./vscode-light.css";
+import styleVscodeDark from "./vscode-dark.css?inline";
+import styleVscodeDefault from "./vscode-default.css?inline";
+import styleVscodeLight from "./vscode-light.css?inline";
 
 export function VSCodeWebProvider({ children }: { children: React.ReactNode }) {
-  return children;
+  return (
+    <div>
+      <style scoped>
+        {styleDefault}
+        {styleVscodeDefault}
+        {styleVscodeDark}
+        {styleVscodeLight}
+      </style>
+      {children}
+    </div>
+  );
 }
