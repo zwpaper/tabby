b8315086d5328abf2809f15c69addcf31682da0a
aliang <1098486429@qq.com>
2025-06-04 23:56:09 +0800

    fix(vscode-webui): adjust error message style (#497)

--- packages/vscode-webui/src/components/error-message.tsx
+++ packages/vscode-webui/src/components/error-message.tsx
@@ -1,5 +1,6 @@
 import { useIsDevMode } from "@/features/settings";
 import { cn } from "@/lib/utils";
+import { ScrollArea } from "./ui/scroll-area";
 
 interface ErrorMessageProps {
   error: Error | undefined;
@@ -9,14 +10,15 @@ export const ErrorMessage: React.FC<ErrorMessageProps> = ({ error }) => {
   const [isDevMode] = useIsDevMode();
   return (
     error && (
-      <div
-        className={cn("mb-2 text-center text-error", {
+      <ScrollArea
+        className={cn("mb-2 break-all text-center text-error", {
           "cursor-help": isDevMode,
         })}
+        viewportClassname="max-h-32"
         onClick={isDevMode ? () => console.error(error) : undefined}
       >
         {error.message}
-      </div>
+      </ScrollArea>
     )
   );
 };
