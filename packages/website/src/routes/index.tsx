5c9ec5f9e699f5b5ea4bbc15069cb5d08e6efec4
aliang <1098486429@qq.com>
2025-07-17 17:56:09 +0800

    chore(website): correct the redirect to home page (#1095)

--- packages/website/src/routes/index.tsx
+++ packages/website/src/routes/index.tsx
@@ -3,7 +3,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   loader: async ({ context }) => {
-    if (context.auth.user) {
+    if (context.auth?.user) {
       throw redirect({
         to: "/home",
       });
