da5911debb5db336974b0e7c284b91fbcae9d7dd
Meng Zhang <meng@tabbyml.com>
2025-10-17 13:32:30 -0700

    refactor(sync-backend): extract payload verification logic into separate function

--- packages/livekit-cf/src/do/sync-backend.ts
+++ packages/livekit-cf/src/do/sync-backend.ts
@@ -4,16 +4,19 @@ import * as SyncBackend from "@livestore/sync-cf/cf-worker";
 
 export class SyncBackendDO extends SyncBackend.makeDurableObject({
   onPush: async (_message, { storeId, payload }) => {
-    if (!(await verifyStoreId(payload, storeId))) {
-      throw new Error("Unauthorized");
-    }
+    await verifyPayload(storeId, payload);
     const id = env.CLIENT_DO.idFromName(storeId);
     const stub = env.CLIENT_DO.get(id);
     await stub.signalKeepAlive(storeId);
   },
   onPull: async (_message, { storeId, payload }) => {
-    if (!(await verifyStoreId(payload, storeId))) {
-      throw new Error("Unauthorized");
-    }
+    await verifyPayload(storeId, payload);
   },
 }) {}
+
+async function verifyPayload(storeId: string, payload: unknown) {
+  // If payload is undefined, it means come from internal backend, e.g DO-rpc
+  if (payload && !(await verifyStoreId(payload, storeId))) {
+    throw new Error("Unauthorized");
+  }
+}
